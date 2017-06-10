1639a905b4da26d9fc26028dcb9c67a9ec8665e3
Sebastian Webber <sebastian@swebber.me>
2017-06-10 19:51:43 -0300

    fix iddentation

--- lib/hba.rb
+++ lib/hba.rb
@@ -5,12 +5,23 @@ class Hba
     def parse_local line
         HbaRule.new :conn_type => line[0], :db_name => line[1], :user_name => line[2], :auth_type => line[3]
     end
+
     def parse_host line
         if line.length == 5
             address = line[3].split("/")
-            HbaRule.new :conn_type => line[0], :db_name => line[1], :user_name => line[2], :ip_mask => address[0], :net_mask => address[1], :auth_type => line[4]
+            HbaRule.new :conn_type => line[0], 
+                        :db_name => line[1], 
+                        :user_name => line[2], 
+                        :ip_mask => address[0], 
+                        :net_mask => address[1], 
+                        :auth_type => line[4]
         else 
-            HbaRule.new :conn_type => line[0], :db_name => line[1], :user_name => line[2], :ip_mask => line[3], :net_mask => line[4], :auth_type => line[5]
+            HbaRule.new :conn_type => line[0], 
+                        :db_name => line[1], 
+                        :user_name => line[2], 
+                        :ip_mask => line[3], 
+                        :net_mask => line[4], 
+                        :auth_type => line[5]
         end
     end
     
@@ -18,7 +29,7 @@ class Hba
     def parse_line line_content
         line = line_content.split
         if line[0] == "local"
-            rule =  parse_local line
+            rule = parse_local line
         else 
             rule = parse_host line
         end

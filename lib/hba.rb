e04fba1a0d0d52472e23b07e87226571fb6fc740
Sebastian Webber <sebastian@swebber.me>
2017-06-10 20:02:06 -0300

    fixing line break

--- lib/hba.rb
+++ lib/hba.rb
@@ -34,6 +34,8 @@ class Hba
             rule = parse_host line
         end
 
+        puts rule.inspect
+
         rule
     end
 
--- lib/rules.rb
+++ lib/rules.rb
@@ -1,5 +1,12 @@
 class HbaRule
-    attr_accessor :line_no, :conn_type, :db_name, :user_name, :ip_addr, :net_mask, :auth_type, :comment
+    attr_accessor   :line_no, 
+                    :conn_type, 
+                    :db_name, 
+                    :user_name, 
+                    :ip_addr, 
+                    :net_mask, 
+                    :auth_type, 
+                    :comment
 
     def initialize args
         args.each do |k,v|

6852ed1f5878b9ef78ba82ada3c0b7ec58c628c2
Sebastian Webber <sebastian@swebber.me>
2017-06-10 18:48:19 -0300

    starting parse file

--- lib/hba.rb
+++ lib/hba.rb
@@ -1,10 +1,18 @@
+require_relative "rules"
+
 class Hba
+    def parse_line line_content
+        line_content
+    end
+
     def read_file file_name
-        File.read file_name
+        File.read(file_name).each_line do |line|
+            puts(line) unless line.strip.start_with?("#")
+        end
     end
     
     def process_file file_name
         puts "processing #{file_name}"
-        puts File.read file_name
+        read_file file_name
     end
 end

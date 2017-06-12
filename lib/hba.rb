710a1a69e758220952437281805aef2d539d573e
Sebastian Webber <sebastian@swebber.me>
2017-06-11 21:17:17 -0300

    adding line_no reference

--- lib/hba.rb
+++ lib/hba.rb
@@ -2,17 +2,22 @@ require_relative "rules"
 
 class Hba
 
-    def parse_local line
-        HbaRule.new :conn_type => line[0], :db_name => line[1], :user_name => line[2], :auth_type => line[3]
+    def parse_local line_no, line
+        HbaRule.new :line_no => line_no,
+                    :conn_type => line[0], 
+                    :db_name => line[1], 
+                    :user_name => line[2], 
+                    :auth_type => line[3]
     end
 
-    def parse_host line
+    def parse_host line_no, line
         if line[3].include? "/"
             address = line[3].split("/")
             line[3] = address[0]
             line.insert(4,address[1])
         end
-        HbaRule.new :conn_type => line[0], 
+        HbaRule.new :line_no => line_no,
+                    :conn_type => line[0], 
                     :db_name => line[1], 
                     :user_name => line[2], 
                     :ip_mask => line[3], 
@@ -21,12 +26,12 @@ class Hba
     end
     
 
-    def parse_line line_content
+    def parse_line line_no, line_content
         line = line_content.split
         if line[0] == "local"
-            rule = parse_local line
+            rule = parse_local line_no, line
         else 
-            rule = parse_host line
+            rule = parse_host line_no, line
         end
 
         puts rule.inspect
@@ -35,8 +40,10 @@ class Hba
     end
 
     def read_file file_name
+        line_no = 0
         File.read(file_name).each_line do |line|
-            puts parse_line(line) unless line.strip.start_with?("#") or line.strip == ""
+            line_no += 1
+            puts parse_line(line_no, line) unless line.strip.start_with?("#") or line.strip == ""
         end
     end
     

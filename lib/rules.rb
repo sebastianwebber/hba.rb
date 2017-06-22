ec6cc9719d854bad222b11c94f5eefa1b79607d0
Sebastian Webber <sebastian@swebber.me>
2017-06-21 22:18:11 -0300

    adding to_s, some default values and starting validation

--- lib/rules.rb
+++ lib/rules.rb
@@ -12,6 +12,16 @@ class HbaRule
         args.each do |k,v|
             instance_variable_set("@#{k}", v) unless v.nil?
         end
+
+        @conn_type ||= "local"
+        @user_name ||= "all"
+        @db_name ||= "all"
+        @auth_type ||= "md5"
+    end
+
+    def to_s
+        "#{@line_no}:#{@conn_type}\t#{@db_name}\t#{@user_name}\t#{@auth_type}"
+        "#{@line_no}:#{@conn_type}\t#{@db_name}\t#{@user_name}\t#{@ip_addr}/#{@net_mask}\t#{@auth_type}" unless [ 'host', 'hostssl'].include?(:conn_type)
     end
 
     def ==(other)
@@ -20,6 +30,11 @@ class HbaRule
 
     def state
         self.instance_variables.map { |variable| self.instance_variable_get variable }
+    end
+
+    def valid?
+        true
+        false unless [ 'local', 'host', 'hostssl'].include?(:conn_type)
     end
 
 end
